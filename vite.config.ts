5f045027dcc3265b66f17fc87956ea40a46128dc
Aleksandr Bykov <azbykov@gmail.com>
2025-11-23 23:24:39 +0400

    init react/vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'zustand-vendor': ['zustand'],
+        },
+      },
+    },
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'esnext',
+  },
+})
